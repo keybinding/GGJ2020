ab4b71b03776138b4919e2c9d49bbd83130d1990
ISTAnsver <spoom95@gmail.com>
2020-02-02 13:26:02 +0300

    Add assets

--- Assets/Scenes/MirrorBehaviour.cs
+++ Assets/Scenes/MirrorBehaviour.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class MirrorBehaviour : MonoBehaviour
 {
     private Camera _camera;
-
+    public Transform MirrorCam;
+    public Transform PlayerCam;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,11 @@ public class MirrorBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 dir = (PlayerCam.position - transform.position).normalized;
+        Quaternion rot = Quaternion.LookRotation(dir);
+
+        rot.eulerAngles = transform.eulerAngles - rot.eulerAngles;
+
+        MirrorCam.rotation = rot;
     }
 }
